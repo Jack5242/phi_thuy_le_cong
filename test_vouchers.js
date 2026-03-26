b49ac7ede9f288a772b4dd75027f8c887faedbc3
Jack5242 <huynhgiahuy071003@gmail.com>
2026-03-26 10:13:11 +0700

    delete test

--- test_vouchers.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-http.get('http://localhost:3000/api/vouchers/available?email=admin@teal.com', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log('RESPONSE:', data));
-}).on('error', err => console.error(err));
